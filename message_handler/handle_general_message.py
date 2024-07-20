2320a3f21e5e93bb18ab89f246d5ee5b41648372
Maoyin Sun <simonmysun@gmail.com>
2024-07-20 06:09:52 +0200

    use variable for message intervals

--- message_handler/handle_general_message.py
+++ message_handler/handle_general_message.py
@@ -11,6 +11,7 @@ from .fetch_content import fetch_content
 from .llm_api import complete
 
 MAX_INPUT_LENGTH = int(os.getenv('MAX_INPUT_LENGTH'))
+message_interval = 0.500
 
 def get_url_from_message(messages: 'telegram.Message[]') -> str:
   all_text = ''
@@ -43,7 +44,7 @@ async def handle_general_message(update: 'telegram.Update', context: 'telegram.e
   replyMessage = await update.message.reply_text('_Processing..._', parse_mode='Markdown')
   url = get_url_from_message([update.message.reply_to_message, update.message])
   if url == '':
-    time.sleep(0.100)
+    time.sleep(message_interval)
     await replyMessage.edit_text('*ERROR*: No URL found in the message.', parse_mode='Markdown')
     return
   logging.info(f'Processing: {url}')
@@ -51,14 +52,14 @@ async def handle_general_message(update: 'telegram.Update', context: 'telegram.e
   content = await fetch_content(uri.geturl())
   if len([line for line in content.split('\n') if line.strip()]) == 0:
     logging.error(f'No content or discussion is fetched. Task aborted.')
-    time.sleep(0.100)
+    time.sleep(message_interval)
     await replyMessage.edit_text('*ERROR*: No content or discussion is fetched. Task aborted.', parse_mode='Markdown')
     return
   prompt = prompt_template_summarize_content.format(**{
     'content': content
   })
   if len(prompt) > MAX_INPUT_LENGTH:
-    time.sleep(0.100)
+    time.sleep(message_interval)
     await replyMessage.edit_text('_Processing..._ (content is truncated)', parse_mode='Markdown')
     logging.info(f'Prompt length is ({len(prompt)} characters). Truncating to {MAX_INPUT_LENGTH} characters.')
     prompt = prompt[:MAX_INPUT_LENGTH]
@@ -69,7 +70,7 @@ async def handle_general_message(update: 'telegram.Update', context: 'telegram.e
   try:
     async for token in complete(prompt):
       result.append(token)
-      if last_sent + 0.100 < time.time():
+      if last_sent + message_interval < time.time():
         last_sent = time.time()
         try:
           await update.message.reply_chat_action(constants.ChatAction.TYPING)
@@ -78,10 +79,10 @@ async def handle_general_message(update: 'telegram.Update', context: 'telegram.e
           pass
   except Exception as e:
     logging.error(f'ERROR: {repr(e)}')
-    time.sleep(0.100)
+    time.sleep(message_interval)
     await replyMessage.edit_text(f'{''.join(result)}\n*ERROR*: LLM API request failed: {repr(e)}', parse_mode='Markdown')
     return
-  time.sleep(0.100)
+  time.sleep(message_interval)
   if len(result) == 0:
     logging.error('No result returned.')
     await replyMessage.edit_text('*ERROR*: No result returned.', parse_mode='Markdown')
@@ -91,12 +92,12 @@ async def handle_general_message(update: 'telegram.Update', context: 'telegram.e
   
   discussion = ''
   if discussion_uri:
-    time.sleep(0.100)
+    time.sleep(message_interval)
     replyMessage = await update.message.reply_text('_Processing discussion_... ', parse_mode='Markdown')
     discussion = await fetch_content(discussion_uri.geturl())
     if len([line for line in discussion.split('\n') if line.strip()]) == 0:
       logging.error(f'No discussion is fetched. Task aborted.')
-      time.sleep(0.100)
+      time.sleep(message_interval)
       await replyMessage.edit_text('*ERROR*: No discussion is fetched. Task aborted.', parse_mode='Markdown')
       return
     prompt = prompt_template_summarize_discussion.format(**{
@@ -114,7 +115,7 @@ async def handle_general_message(update: 'telegram.Update', context: 'telegram.e
     try:
       async for token in complete(prompt):
         result.append(token)
-        if last_sent + 0.100 < time.time():
+        if last_sent + message_interval < time.time():
           last_sent = time.time()
           try:
             await update.message.reply_chat_action(constants.ChatAction.TYPING)
@@ -123,10 +124,10 @@ async def handle_general_message(update: 'telegram.Update', context: 'telegram.e
             pass
     except Exception as e:
       logging.error(f'ERROR: {repr(e)}')
-      time.sleep(0.100)
+      time.sleep(message_interval)
       await replyMessage.edit_text(f'{''.join(result)}\n*ERROR*: LLM API request failed: {repr(e)}', parse_mode='Markdown')
       return
-    time.sleep(0.100)
+    time.sleep(message_interval)
     if len(result) == 0:
       logging.error('No result returned.')
       await replyMessage.edit_text('*ERROR*: No result returned.', parse_mode='Markdown')
