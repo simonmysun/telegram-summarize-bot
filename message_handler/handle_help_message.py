232efcd96104f160299e89f2c4cf4eec77c5b65f
Maoyin Sun <simonmysun@gmail.com>
2024-09-22 22:31:52 +0200

    remove unused code

--- message_handler/handle_help_message.py
+++ message_handler/handle_help_message.py
@@ -5,7 +5,6 @@ if typing.TYPE_CHECKING:
 import os, logging
 
 ADMIN_USER_IDS = list(map(int, os.getenv('ADMIN_USER_IDS').split(',')))
-ALLOWED_TELEGRAM_USER_IDS = list(map(int, os.getenv('ALLOWED_TELEGRAM_USER_IDS').split(',')))
 
 async def handle_help_message(update: 'telegram.Update', context: 'telegram.ext.CallbackContext') -> None:
   userId = update.message.from_user.id
@@ -15,9 +14,6 @@ async def handle_help_message(update: 'telegram.Update', context: 'telegram.ext.
     ('help', 'Show this help message')
   ])
   logging.info(await context.bot.get_my_commands())
-  if userId in ADMIN_USER_IDS or userId in ALLOWED_TELEGRAM_USER_IDS:
-    await update.message.reply_text('I summarize the first link in the message')
+  await update.message.reply_text('I summarize the first link in the message')
   if userId in ADMIN_USER_IDS:
     await update.message.reply_text('Admin commands are available.')
-  else:
-    await update.message.reply_text('Permission denied')
