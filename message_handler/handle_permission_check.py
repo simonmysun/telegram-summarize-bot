f93a1be307b3c1c5ee63b895d0422f302b701a44
Maoyin Sun <simonmysun@gmail.com>
2025-01-07 18:15:42 +0100

    log chat_id when message arrives

--- message_handler/handle_permission_check.py
+++ message_handler/handle_permission_check.py
@@ -15,7 +15,7 @@ ALLOWED_TELEGRAM_USER_IDS = list(map(int, os.getenv('ALLOWED_TELEGRAM_USER_IDS')
 async def handle_permission_check(update: 'telegram.Update', context: 'telegram.ext.CallbackContext') -> None:
   user_id = update.message.from_user.id
   chat_id = update.message.chat.id
-  logger.info(f'user<{user_id}>: {update.message.text}')
+  logger.info(f'user<{user_id}>@chat<{chat_id}>: {update.message.text}')
   if update.message.chat.type not in ['GROUP', 'SUPERGROUP'] and user_id not in ADMIN_USER_IDS and user_id not in ALLOWED_TELEGRAM_USER_IDS:
     logger.info(f'user<{user_id}>@chat<{chat_id}>: Permission denied')
     await update.message.reply_text('Permission denied')
