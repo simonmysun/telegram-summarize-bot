303a431327944fec6229ff1e8a5f76604519659a
Maoyin Sun <simonmysun@gmail.com>
2025-01-16 20:46:56 +0100

    use correct constants for group chats

--- message_handler/handle_permission_check.py
+++ message_handler/handle_permission_check.py
@@ -7,6 +7,7 @@ import typing
 if typing.TYPE_CHECKING:
     import telegram
 
+import telegram
 from telegram.ext import ApplicationHandlerStop
 
 ADMIN_USER_IDS = list(map(int, os.getenv('ADMIN_USER_IDS').split(',')))
@@ -15,8 +16,9 @@ ALLOWED_TELEGRAM_USER_IDS = list(map(int, os.getenv('ALLOWED_TELEGRAM_USER_IDS')
 async def handle_permission_check(update: 'telegram.Update', context: 'telegram.ext.CallbackContext') -> None:
   user_id = update.message.from_user.id
   chat_id = update.message.chat.id
-  logger.info(f'user<{user_id}>@chat<{chat_id}>: {update.message.text}')
-  if update.message.chat.type not in ['GROUP', 'SUPERGROUP'] and user_id not in ADMIN_USER_IDS and user_id not in ALLOWED_TELEGRAM_USER_IDS:
+  if update.message.chat.type not in [
+    telegram.constants.ChatType.GROUP, telegram.constants.ChatType.SUPERGROUP
+    ] and user_id not in ADMIN_USER_IDS and user_id not in ALLOWED_TELEGRAM_USER_IDS:
     logger.info(f'user<{user_id}>@chat<{chat_id}>: Permission denied')
     await update.message.reply_text('Permission denied')
     raise ApplicationHandlerStop()
